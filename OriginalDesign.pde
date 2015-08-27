74610df910779d16a58105b09285d2ae6a6e59a1
unknown <antom3@697-334-Erebor.sf.k12.internal>
2015-08-27 08:06:22 -0700

    Created design

--- OriginalDesign.pde
+++ OriginalDesign.pde
@@ -1,18 +1,58 @@
-int x=250;
-int y=250;
+
+
+
+Diam diam = new Diam();
+int diameter;
+int grow;
+int cirX;
+int cirY;
+int i;
+boolean locked=true;
+
+
 
 void setup(){
 	size(512, 512);
+	background(0);
 
 }
 
 void draw(){
-	background(0);
-	ball();
-	xy();
+	display();
+	if (locked==false) {
+		diam.update(diameter);
+		diameter=diameter+grow;
+	} 
+
+	
 }
 
 
-void mouseClick() {
-	if (mouseButton == LEFT) {}
+void display() {
+	println(locked);
 }
+
+void mousePressed() {
+	
+	if (mouseButton == LEFT && locked == true) {
+		locked=false;
+		grow=1;
+		cirX=mouseX;
+		cirY=mouseY;
+		redraw();
+	} else if (mouseButton == RIGHT && locked == false) {
+		locked=true;
+		grow = 0;
+		diameter = 0;
+	}
+}
+
+class Diam {
+	void update(int y) {
+		fill ((int)(Math.random()*y),255-y,y);
+		ellipse(cirX,cirY,y,y);
+	}
+}
+	
+
+
--- build-tmp/source/OriginalDesign.java
+++ build-tmp/source/OriginalDesign.java
@@ -14,39 +14,64 @@ import java.io.IOException;
 
 public class OriginalDesign extends PApplet {
 
-int x=250;
-int y=250;
+
+
+
+Diam diam = new Diam();
+int diameter;
+int grow;
+int cirX;
+int cirY;
+int i;
+boolean locked=true;
+
+
 
 public void setup(){
 	size(512, 512);
+	background(0);
 
 }
 
 public void draw(){
-	background(0);
-	ball();
-	xy();
-}
+	display();
+	if (locked==false) {
+		diam.update(diameter);
+		diameter=diameter+grow;
+	} 
 
-public void ball() {
-	ellipse (x,y, 50, 50);
 	
 }
 
-public void xy() {
-	if (x <= 0) {
-		x++;
-	} else {
-		x--;
-	}
-	 
 
-	if (y <= 0) {
-		y++;
-	} else if (y>500){
-		y--;
+public void display() {
+	println(locked);
+}
+
+public void mousePressed() {
+	
+	if (mouseButton == LEFT && locked == true) {
+		locked=false;
+		grow=1;
+		cirX=mouseX;
+		cirY=mouseY;
+		redraw();
+	} else if (mouseButton == RIGHT && locked == false) {
+		locked=true;
+		grow = 0;
+		diameter = 0;
 	}
 }
+
+class Diam {
+	public void update(int y) {
+		fill ((int)(Math.random()*y),255-y,y);
+		ellipse(cirX,cirY,y,y);
+	}
+}
+	
+
+
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "OriginalDesign" };
     if (passedArgs != null) {
